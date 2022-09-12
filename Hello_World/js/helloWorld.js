4775e282c96781a60125dffa6f613cbe9ce1da6c
BM21008601 <90199966+BM21008601@users.noreply.github.com>
2022-09-12 15:33:53 +0100

    Add files via upload

---
+++ Hello_World/js/helloWorld.js
@@ -0,0 +1,7 @@
+var surname; //creates the surname variable
+var forename;   //creates the forename variable
+
+surname = "World"; //assigns World to the surname variable
+forename = "Hello"; //assigns Hello to the forename variable
+
+window.alert(forename + " " + surname); //displays "Hello World"
